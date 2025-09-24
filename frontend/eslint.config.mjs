84c84027a40869dfacaa175e80b7a93801fc6b1b
sik2 <bickwang123@gmail.com>
2025-09-24 15:24:37 +0900

    17

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -18,6 +18,9 @@ const eslintConfig = [
       'out/**',
       'build/**',
       'next-env.d.ts',
+      'src/lib/backend/*/schema.d.ts', // openapi-typescript에 의해서 자동으로 만들어지는 파일들이 저장되는 경로
+      'src/components/**', // 추후 샤드CN에 의해서 자동으로 만들어지는 파일들이 저장될 경로
+      'src/hooks/**', // 추후 샤드CN에 의해서 자동으로 만들어지는 파일들이 저장될 경로
     ],
   },
 ]
