ac03d63536f2beb7f93e3903f386fbbe845ca12d
sik2 <bickwang123@gmail.com>
2025-09-25 11:28:18 +0900

    29-2

--- frontend/src/lib/backend/client.ts
+++ frontend/src/lib/backend/client.ts
@@ -9,22 +9,3 @@ export const client = createClient<paths>({
   baseUrl: API_BASE_URL,
   credentials: 'include',
 })
-
-// fetch 기반의 apiFetch
-export const apiFetch = (url: string, options?: RequestInit) => {
-  options = options || {}
-  options.credentials = 'include'
-
-  if (options?.body) {
-    const headers = new Headers(options.headers || {})
-
-    // 이미 호출하는 쪽에서 Content-Type을 지정했다면 덮어쓰지 않고 그대로 사용
-    if (!headers.has('Content-Type')) {
-      headers.set('Content-Type', 'application/json')
-    }
-
-    options.headers = headers
-  }
-
-  return fetch(`${API_BASE_URL}${url}`, options).then((res) => res.json())
-}
