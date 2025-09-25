5726f33e730bc94d99439850f7eed26e6b6f04e9
sik2 <bickwang123@gmail.com>
2025-09-25 14:45:20 +0900

    38-1

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,12 +1,28 @@
 'use client'
 
+import { client } from '@/global/backend/client'
+
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const router = useRouter()
+
+  const logout = () => {
+    client.DELETE('/api/v1/members/logout').then((res) => {
+      if (res.error) {
+        alert(res.error.msg)
+        return
+      }
+
+      router.replace('/posts')
+    })
+  }
+
   return (
     <>
       <header>
@@ -20,6 +36,9 @@ export default function ClientLayout({
           <Link href="/members/login" className="p-2 rounded hover:bg-gray-100">
             로그인
           </Link>
+          <button onClick={logout} className="p-2 rounded hover:bg-gray-100">
+            로그아웃
+          </button>
         </nav>
       </header>
       <main className="flex-1 flex flex-col">{children}</main>
