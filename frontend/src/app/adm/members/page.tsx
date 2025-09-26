18cb5b1ead666010a9770b620d7b147198332d60
sik2 <bickwang123@gmail.com>
2025-09-26 11:41:59 +0900

    48

---
+++ frontend/src/app/adm/members/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { components } from '@/global/backend/apiV1/schema'
+import { client } from '@/global/backend/client'
+import { useEffect, useState } from 'react'
+
+import Link from 'next/link'
+
+type MemberWithUsernameDto = components['schemas']['MemberWithUsernameDto']
+
+export default function Page() {
+  const [members, setMembers] = useState<MemberWithUsernameDto[] | null>(null)
+
+  useEffect(() => {
+    client
+      .GET('/api/v1/adm/members')
+      .then((res) => res.data && setMembers(res.data))
+  }, [])
+
+  if (members == null) return <div>로딩중...</div>
+
+  return (
+    <>
+      <h1>회원 목록</h1>
+
+      {members.length == 0 && <div>회원이 없습니다.</div>}
+
+      {members.length > 0 && (
+        <ul>
+          {members.map((member) => (
+            <li key={member.id}>
+              <Link href={`/members/${member.id}`}>
+                {member.id} : {member.username} / {member.nickname}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  )
+}
